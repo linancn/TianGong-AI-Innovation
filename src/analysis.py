7ec172b0c090cbcb83f5969b24d4506c237fad9a
nanli <linanenv@gmail.com>
2024-01-09 14:09:39 +0800

    udpate

--- src/analysis.py
+++ src/analysis.py
@@ -12,20 +12,20 @@ numeric_cols = df.select_dtypes(include=[float]).columns
 grouped_df = df.groupby("uuid")[numeric_cols].mean().reset_index()
 
 # grouped_df_t = grouped_df.transpose()
-grouped_df["Sum"] = df.iloc[:, 1:32].sum(axis=1)
-grouped_df["Mean"] = df.iloc[:, 1:32].mean(axis=1)
-grouped_df["Median"] = df.iloc[:, 1:32].median(axis=1)
-grouped_df["Std"] = df.iloc[:, 1:32].std(axis=1)
-grouped_df["Min"] = df.iloc[:, 1:32].min(axis=1)
-grouped_df["Max"] = df.iloc[:, 1:32].max(axis=1)
+grouped_df["Sum"] = grouped_df.iloc[:, 1:33].sum(axis=1)
+grouped_df["Mean"] = grouped_df.iloc[:, 1:33].mean(axis=1)
+grouped_df["Median"] = grouped_df.iloc[:, 1:33].median(axis=1)
+grouped_df["Std"] = grouped_df.iloc[:, 1:33].std(axis=1)
+grouped_df["Min"] = grouped_df.iloc[:, 1:33].min(axis=1)
+grouped_df["Max"] = grouped_df.iloc[:, 1:33].max(axis=1)
 grouped_df["Range"] = grouped_df["Max"] - grouped_df["Min"]
-grouped_df["Variance"] = df.iloc[:, 1:32].var(axis=1)
-grouped_df["Skewness"] = df.iloc[:, 1:32].skew(axis=1)
-grouped_df["Kurtosis"] = df.iloc[:, 1:32].kurt(axis=1)
-grouped_df["Quantile_25"] = df.iloc[:, 1:32].quantile(q=0.25, axis=1)
-grouped_df["Quantile_50"] = df.iloc[:, 1:32].quantile(q=0.5, axis=1)
-grouped_df["Quantile_75"] = df.iloc[:, 1:32].quantile(q=0.75, axis=1)
-grouped_df["Quantile_90"] = df.iloc[:, 1:32].quantile(q=0.9, axis=1)
+grouped_df["Variance"] = grouped_df.iloc[:, 1:33].var(axis=1)
+grouped_df["Skewness"] = grouped_df.iloc[:, 1:33].skew(axis=1)
+grouped_df["Kurtosis"] = grouped_df.iloc[:, 1:33].kurt(axis=1)
+grouped_df["Quantile_25"] = grouped_df.iloc[:, 1:33].quantile(q=0.25, axis=1)
+grouped_df["Quantile_50"] = grouped_df.iloc[:, 1:33].quantile(q=0.5, axis=1)
+grouped_df["Quantile_75"] = grouped_df.iloc[:, 1:33].quantile(q=0.75, axis=1)
+grouped_df["Quantile_90"] = grouped_df.iloc[:, 1:33].quantile(q=0.9, axis=1)
 
 
 def create_engine_pg():
